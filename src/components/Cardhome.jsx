92d5a2955ff4d8a35cd74c3b76fd08958285aaff
Amit Kavathekar <amitkavathekar123@gmail.com>
2025-09-16 17:26:17 +0530

    make commit

---
+++ src/components/Cardhome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cardhome = () => {
+  return (
+    <div>Cardhome</div>
+  )
+}
+
+export default Cardhome
---
+++ src/pages/ForCorporates.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForCorporates = () => {
+  return (
+    <div>ForCorporates</div>
+  )
+}
+
+export default ForCorporates
